75278c4aa80aa4c83830f72df9cbb43d323d5cbf
Kim Chi Tran <146179578+kimchitran-cap@users.noreply.github.com>
2024-09-26 18:13:10 +0200

    Adapt backend and frontend to show list of saved requests

--- backend/src/main/java/eu/possiblex/portal/business/control/ParticipantRegistrationService.java
+++ backend/src/main/java/eu/possiblex/portal/business/control/ParticipantRegistrationService.java
@@ -1,8 +1,13 @@
 package eu.possiblex.portal.business.control;
 
+import eu.possiblex.portal.application.entity.RegistrationRequestListTO;
 import eu.possiblex.portal.business.entity.PossibleParticipantBE;
+
+import java.util.List;
 
 public interface ParticipantRegistrationService {
 
     void registerParticipant(PossibleParticipantBE be);
+
+    List<RegistrationRequestListTO> getAllParticipantRegistrationRequests();
 }
--- frontend/src/app/views/administration/registration-request-management/registration-request-management.component.ts
+++ frontend/src/app/views/administration/registration-request-management/registration-request-management.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {ApiService} from "../../../services/mgmt/api/api.service";
+import {IRegistrationRequestListTO} from "../../../services/mgmt/api/backend";
 
 @Component({
   selector: 'app-registration-request-management',
@@ -6,19 +8,12 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./registration-request-management.component.scss']
 })
 export class RegistrationRequestManagementComponent implements OnInit{
-  registrationRequests: any[] = [];
+  registrationRequests: IRegistrationRequestListTO[] = [];
+
+  constructor(private apiService: ApiService) {}
 
   async getRegistrationRequests() {
-    this.registrationRequests = [{"name":"orga1", "description":"desc1", "address":{"street":"street1", "city":"city1", "zip":"zip1"}},
-      {"name":"orga2", "description":"desc2", "address":{"street":"street2", "city":"city2", "zip":"zip2"}},
-      {"name":"orga3", "description":"desc3", "address":{"street":"street3", "city":"city3", "zip":"zip3"}},
-      {"name":"orga4", "description":"desc4", "address":{"street":"street4", "city":"city4", "zip":"zip4"}},
-      {"name":"orga5", "description":"desc5", "address":{"street":"street5", "city":"city5", "zip":"zip5"}},
-      {"name":"orga6", "description":"desc6", "address":{"street":"street6", "city":"city6", "zip":"zip6"}},
-      {"name":"orga7", "description":"desc7", "address":{"street":"street7", "city":"city7", "zip":"zip7"}},
-      {"name":"orga8", "description":"desc8", "address":{"street":"street8", "city":"city8", "zip":"zip8"}},
-      {"name":"orga9", "description":"desc9", "address":{"street":"street9", "city":"city9", "zip":"zip9"}},
-      {"name":"orga10", "description":"desc10", "address":{"street":"street10", "city":"city10", "zip":"zip10"}}];
+    this.registrationRequests = await this.apiService.getAllRegistrationRequests();
   }
 
   ngOnInit(): void {
