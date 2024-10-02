b37b6572fc8b064861735e524e8734c8aa7e4a43
M-Busk <125284318+M-Busk@users.noreply.github.com>
2024-10-02 14:51:32 +0200

    implement in-memory db for dao test

--- backend/src/test/java/eu/possiblex/portal/persistence/dao/ParticipantRegistrationRequestDAOTest.java
+++ backend/src/test/java/eu/possiblex/portal/persistence/dao/ParticipantRegistrationRequestDAOTest.java
@@ -4,20 +4,20 @@ import eu.possiblex.portal.application.entity.credentials.gx.datatypes.GxVcard;
 import eu.possiblex.portal.application.entity.credentials.gx.participants.GxLegalRegistrationNumberCredentialSubject;
 import eu.possiblex.portal.business.entity.credentials.px.PxExtendedLegalParticipantCredentialSubject;
 import eu.possiblex.portal.persistence.control.ParticipantRegistrationEntityMapper;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
-import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
-public class ParticipantRegistrationRequestDAOTest {
-    @MockBean
+@Transactional
+class ParticipantRegistrationRequestDAOTest {
+    @SpyBean
     private ParticipantRegistrationRequestRepository participantRegistrationRequestRepository;
 
     @Autowired
@@ -28,6 +28,7 @@ public class ParticipantRegistrationRequestDAOTest {
 
     @Test
     void saveParticipantRegistrationRequest() {
+
         PxExtendedLegalParticipantCredentialSubject participant = getParticipant();
         participantRegistrationRequestDAO.saveParticipantRegistrationRequest(participant);
         verify(participantRegistrationRequestRepository).save(any());
@@ -35,34 +36,36 @@ public class ParticipantRegistrationRequestDAOTest {
 
     @Test
     void getAllParticipantRegistrationRequests() {
+
         participantRegistrationRequestDAO.getAllParticipantRegistrationRequests();
         verify(participantRegistrationRequestRepository).findAll();
     }
 
     @Test
     void acceptRegistrationRequest() {
+
         PxExtendedLegalParticipantCredentialSubject participant = getParticipant();
 
-        when(participantRegistrationRequestRepository.findByName("validId")).thenReturn(participantRegistrationEntityMapper.pxExtendedLegalParticipantCsToEntity(participant));
         participantRegistrationRequestDAO.saveParticipantRegistrationRequest(participant);
-        participantRegistrationRequestDAO.acceptRegistrationRequest("validId");
+        participantRegistrationRequestDAO.acceptRegistrationRequest("validName");
         verify(participantRegistrationRequestRepository, times(2)).save(any());
     }
 
     @Test
     void rejectAndDeleteRegistrationRequest() {
+
         PxExtendedLegalParticipantCredentialSubject participant = getParticipant();
 
-        when(participantRegistrationRequestRepository.findByName("validId")).thenReturn(participantRegistrationEntityMapper.pxExtendedLegalParticipantCsToEntity(participant));
         participantRegistrationRequestDAO.saveParticipantRegistrationRequest(participant);
-        participantRegistrationRequestDAO.rejectRegistrationRequest("validId");
-        participantRegistrationRequestDAO.deleteRegistrationRequest("validId");
+        participantRegistrationRequestDAO.rejectRegistrationRequest("validName");
+        participantRegistrationRequestDAO.deleteRegistrationRequest("validName");
         verify(participantRegistrationRequestRepository, times(2)).save(any());
 
         verify(participantRegistrationRequestRepository).delete(any());
     }
 
     private PxExtendedLegalParticipantCredentialSubject getParticipant() {
+
         GxVcard vcard = new GxVcard();
         vcard.setCountryCode("validCountryCode");
         vcard.setCountrySubdivisionCode("validSubdivisionCode");
@@ -70,21 +73,8 @@ public class ParticipantRegistrationRequestDAOTest {
         vcard.setLocality("validLocality");
         vcard.setPostalCode("validPostalCode");
 
-        return PxExtendedLegalParticipantCredentialSubject.builder().id("validId")
-                .legalRegistrationNumber(new GxLegalRegistrationNumberCredentialSubject("validEori", "validVatId", "validLeiCode"))
-                .headquarterAddress(vcard)
-                .name("validName")
-                .description("validDescription")
-                .build();
-    }
-
-    // Test-specific configuration to provide mocks
-    @TestConfiguration
-    static class TestConfig {
-        @Bean
-        public ParticipantRegistrationEntityMapper participantRegistrationEntityMapper() {
-
-            return Mappers.getMapper(ParticipantRegistrationEntityMapper.class);
-        }
+        return PxExtendedLegalParticipantCredentialSubject.builder().id("validId").legalRegistrationNumber(
+                new GxLegalRegistrationNumberCredentialSubject("validEori", "validVatId", "validLeiCode"))
+            .headquarterAddress(vcard).name("validName").description("validDescription").build();
     }
 }
