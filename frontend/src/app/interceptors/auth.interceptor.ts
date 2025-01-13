0a540d30d2931c300c21a00231b2cd7a287ddcc5
cengiz-yazici <cengiz.yazici@capgemini.com>
2025-01-13 11:26:27 +0100

    [PI-630] fixed the problem with the default login

--- frontend/src/app/interceptors/auth.interceptor.ts
+++ frontend/src/app/interceptors/auth.interceptor.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpErrorResponse, HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, throwError } from 'rxjs';
 import {Router} from "@angular/router";
@@ -10,28 +10,15 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (req.url.includes("/registration/") && !(req.url.match("\/registration\/request$") && req.method === "POST")) {
-      var authToken = sessionStorage.getItem("authToken");
-      if (!authToken) {
-        var username = window.prompt("Enter your username:");
-        var password = window.prompt("Enter your password:");
-        authToken = btoa(`${username}:${password}`);
-        sessionStorage.setItem("authToken", authToken)
-      }
-      var authReq = req.clone({
-        setHeaders: {
-          Authorization: `Basic ${authToken}`
-        }
-      });
-      return next.handle(authReq).pipe(catchError((error: HttpErrorResponse) => {
+      return next.handle(req).pipe(catchError((error: HttpErrorResponse) => {
           if (error.status === 401) {
-            sessionStorage.removeItem('authToken');
             alert('Invalid Token generated. Please log in again.');
-            this.router.navigate(["/"]);
           } else if (error.status === 403) {
-            sessionStorage.removeItem('authToken');
             alert('Unauthorized Token used. Please log in with the correct Role.');
-            this.router.navigate(["/"]);
+          } else {
+            alert('Something went wrong.');
           }
+          this.router.navigate(["/"]);
           return throwError(() => new Error(error.message))
         }));
     } else {
