b7635eb6b1ec69f2d0dab137d09f45b72b844449
Kim Chi Tran <146179578+kimchitran-cap@users.noreply.github.com>
2025-02-19 11:26:11 +0100

    Remove unused import from frontend test

--- frontend/src/app/views/login/login.component.spec.ts
+++ frontend/src/app/views/login/login.component.spec.ts
@@ -17,7 +17,6 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {LoginComponent} from './login.component';
-import {HomeComponent} from "../home/home.component";
 import {RouterTestingModule} from "@angular/router/testing";
 import {ReactiveFormsModule} from '@angular/forms';
 
